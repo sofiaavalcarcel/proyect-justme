7eaea73826ab249d601a288db6783cc594918990
yarii <yarithmanrique@gmail.com>
2026-04-22 23:08:45 -0500

    fase1y2Profesional

--- src/professionals/professionals.module.ts
+++ src/professionals/professionals.module.ts
@@ -22,7 +22,7 @@ import { IncentiveProgram } from '../coupons/entities/incentive-program.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Professional, PortfolioImage, User]),
+        TypeOrmModule.forFeature([Professional, PortfolioImage, User, Booking, Review, Wallet, Transaction, IncentiveProgram]),
         forwardRef(() => ScheduleModule),
         MulterModule.register({
             storage: diskStorage({
